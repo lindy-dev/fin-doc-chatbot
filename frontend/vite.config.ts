04737836c825a643b323cd50e72e6236bb1155c2
lindy-dev <shreyasrao2021@gmail.com>
2026-03-02 19:27:13 +0530

    Added dockerfiles

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5174 // Change the default port here
+    port: 5174,
+    host: "0.0.0.0",
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 80,
+    },
   },
-})
+});
